ae23efee9cb18ddc4b60664ab4f32311e62926bb
Glatigny <f.west@live.fr>
2019-02-21 15:38:59 +0100

    ajout du script angular js

---
+++ assets/js/iphone.js
@@ -0,0 +1,72 @@
+
+
+function responsiveChat(element) {
+    $(element).html('<form class="chat"><span></span><div class="messages"></div><input type="text" placeholder="Your message"><input type="submit" value="Send"></form>');
+
+    function showLatestMessage() {
+        $(element).find('.messages').scrollTop($(element).find('.messages').height());
+    }
+    showLatestMessage();
+
+
+    $(element + ' input[type="text"]').keypress(function (event) {
+        if (event.which == 13) {
+            event.preventDefault();
+            $(element + ' input[type="submit"]').click();
+        }
+    });
+    $(element + ' input[type="submit"]').click(function (event) {
+        event.preventDefault();
+        var message = $(element + ' input[type="text"]').val();
+        if ($(element + ' input[type="text"]').val()) {
+            var d = new Date();
+            var clock = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+            var month = d.getMonth() + 1;
+            var day = d.getDate();
+            var currentDate =
+                (("" + day).length < 2 ? "0" : "") +
+                day +
+                "." +
+                (("" + month).length < 2 ? "0" : "") +
+                month +
+                "." +
+                d.getFullYear() +
+                "&nbsp;&nbsp;" +
+                clock;
+            $(element + ' div.messages').append(
+                '<div class="message"><div class="myMessage"><p>' +
+                message +
+                "</p><date>" +
+                currentDate +
+                "</date></div></div>"
+            );
+            setTimeout(function () {
+                $(element + ' > span').addClass("spinner");
+            }, 100);
+            setTimeout(function () {
+                $(element + ' > span').removeClass("spinner");
+            }, 2000);
+        }
+        $(element + ' input[type="text"]').val("");
+        showLatestMessage();
+    });
+}
+
+function responsiveChatPush(element, sender, origin, date, message) {
+    var originClass;
+    if (origin == 'me') {
+        originClass = 'myMessage';
+    } else {
+        originClass = 'fromThem';
+    }
+    $(element + ' .messages').append('<div class="message"><div class="' + originClass + '"><p>' + message + '</p><date><b>' + sender + '</b> ' + date + '</date></div></div>');
+}
+
+/* Activating chatbox on element */
+responsiveChat('.responsive-html5-chat');
+
+/* Let's push some dummy data */
+responsiveChatPush('.chat', 'Recruteur', 'you', '19.02.2019 14:30', 'Salut Francky! J\'ai une superbe offre à te proposer. Aurais-tu une adresse mail stp? J\'aimerais t\'envoyer la fiche de poste. Si l\'opportunité t\'intéresse, nous pourrions nous appeler cette semaine afin d\'en discuter de vive voix. Qu\'en dis-tu?');
+responsiveChatPush('.chat', 'Francky', 'me', '19.02.2019 14:36', 'Bonjour!<br> Pour avoir mes coordonnées complètes et me joindre, <br><strong>SCANNE LE QR CODE CI-DESSOUS AVEC TON SMARTPHONE : <img src="assets/images/qrcode.png" width="150" height="150" /></strong>');
+responsiveChatPush('.chat', 'Recruteur', 'you', '19.02.2019 14:52', 'QR Code scanné, c\'est dans la boîte, merci ! :)');
+responsiveChatPush('.chat', 'Francky', 'me', '19.02.2019 15:07', 'Je t\'en prie!');
